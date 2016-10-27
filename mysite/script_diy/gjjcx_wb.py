f968dc75b930f659d0777d0c934cb0920183e2af
小贤 <manyueonline@163.com>
2016-10-28 00:19:34 +0800

    查询gjj更新
    验证码的目录地址改为用os.path.dirname&os.path.abspath获取；使用with dosth as f
    替代原来的try...finally

--- mysite/script_diy/gjjcx_wb.py
+++ mysite/script_diy/gjjcx_wb.py
@@ -23,15 +23,19 @@ print('gjjcx go >>>')
 http = urllib3.PoolManager()
 
 img_url = 'http://app.szzfgjj.com:7001/pages/code.jsp'
-img_dir = 'D:/pythonworkspace/gjjcx/verify/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print('base_dir',BASE_DIR)
+img_dir = BASE_DIR+'\\verify\\'
 time = datetime.now().timestamp()*10000000
 img_path_str = img_dir+time.__str__()+".png"
 
 # 获取验证码图片
 r_img = http.request('GET',img_url)
-f_img = open(img_path_str,'wb')
-f_img.write(r_img.data)
-f_img.close()
+with open(img_path_str,'wb') as f_img:
+	f_img.write(r_img.data)
+# f_img = open(img_path_str,'wb')
+# f_img.write(r_img.data)
+# f_img.close()
 
 img_cookie = r_img.headers.get('Set-Cookie')
 print('获取验证码Cookie:',img_cookie)
